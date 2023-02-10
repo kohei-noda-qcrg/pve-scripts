c7cfd7acc5ce0634be58c6b17b5388a09ac41193
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-02-11 08:45:07 +0900

    Create initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Disable pve-enterprise repo and add pve-no-subscription repo
+sed -i -e "s/deb/#deb/" /etc/apt/sources.list.d/pve-enterprise.list
+echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list
+
+# Upgrade softwares
+apt update && apt upgrade
