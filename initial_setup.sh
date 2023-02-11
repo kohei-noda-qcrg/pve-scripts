a183c21cc3cdcf1c057ff20b40b7751b275a5586
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-02-11 14:54:05 +0900

    Add -y option

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,4 +7,4 @@ sed -i -e "s/deb/#deb/" /etc/apt/sources.list.d/pve-enterprise.list
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list
 
 # Upgrade softwares
-apt update && apt upgrade
+apt update -y && apt upgrade -y
